816ac921f5d8a11cacc46105777f3560f890be59
zainarshad16 <zainarshad110@gmail.com>
2025-12-27 22:05:29 +0500

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ReactLenis } from 'lenis/react'
+
+function App() {
+
+ 
+  return (
+    <>
+     <ReactLenis root>
+      {/* Your website content here */}
+    </ReactLenis>
+    </>
+  )
+}
+
+export default App
